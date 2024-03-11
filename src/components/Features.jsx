9bd5520bc8f4f64bbf3a9d38ef7b36558b7c5a2e
rohit <rohitkushwah67838@gmail.com>
2024-03-11 14:31:27 +0530

    message

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -13,7 +13,7 @@ function Features() {
   return (
     <div className="w-full pb-[12vw] overflow-hidden">
       <div className="w-full pt-3 pb-10 mb-[3vw] px-12 border-b-[1px] border-zinc-700">
-        <h1 className="text-[3.8vw] font-['Neue_Montreal']">
+        <h1 className="text-[3.8vw] font-['mont']">
           Featured projects
         </h1>
       </div>
@@ -21,7 +21,7 @@ function Features() {
         <div className="cards w-full flex gap-3">
           <div className="w-1/2 h-[80vh]">
             <div className="flex items-center gap-3 pb-6">
-              <div className="w-3 h-3 bg-[#fff] rounded-full"></div>
+              <div className="w-3 h-3 bg-[#000] rounded-full"></div>
               <div>
                 <p className="uppercase">fyde</p>
               </div>
@@ -32,8 +32,9 @@ function Features() {
               onHoverEnd={() => handelHoverEnd(0)}
               className="cardContainer relative w-full h-full bg-red-100 rounded-lg cursor-pointer"
             >
-              <h1 className="text-9xl text-[#CDEA68] left-full z-[9] absolute font-['Test_Founders_Grotesk_X-Cond_SmBd'] uppercase -translate-x-1/2 top-1/2 -translate-y-1/2 flex overflow-hidden">
+              <h1 className="text-9xl text-[#CDEA68] left-full z-[9] absolute font-['founders'] uppercase -translate-x-1/2 top-1/2 -translate-y-1/2 flex overflow-hidden">
                 {"fyde".split("").map((item, index) => (
+                  // eslint-disable-next-line react/jsx-key
                   <motion.span
                     initial={{ y: "100%" }}
                     animate={cards[0]}
@@ -58,7 +59,8 @@ function Features() {
               <div className="flex w-full items-center gap-4 py-3">
                 {["audit", "copywriting", "sales deck", "slides design"].map(
                   (item, index) => (
-                    <button className="border-[1px] border-zinc-100 px-5 py-1 rounded-full uppercase font-['Neue_Montreal'] text-[13px] hover:bg-white hover:text-[#222] duration-200">
+                    // eslint-disable-next-line react/jsx-key
+                    <button className="border-[1px] border-zinc-100 px-5 py-1 rounded-full uppercase font-['mont'] text-[13px] hover:bg-white hover:text-[#222] duration-200">
                       {item}
                     </button>
                   )
@@ -70,7 +72,7 @@ function Features() {
           </div>
           <div className="w-1/2 h-[80vh]">
             <div className="flex items-center gap-3 pb-6">
-              <div className="w-3 h-3 bg-[#fff] rounded-full"></div>
+              <div className="w-3 h-3 bg-[#000] rounded-full"></div>
               <div>
                 <p className="uppercase">vise</p>
               </div>
@@ -81,8 +83,9 @@ function Features() {
               onHoverEnd={() => handelHoverEnd(1)}
               className="cardContainer relative w-full h-full bg-red-100 rounded-lg cursor-pointer"
             >
-              <h1 className="text-9xl text-[#CDEA68] right-full z-[9] absolute font-['Test_Founders_Grotesk_X-Cond_SmBd'] uppercase translate-x-1/2 top-1/2 -translate-y-1/2 overflow-hidden flex">
+              <h1 className="text-9xl text-[#CDEA68] right-full z-[9] absolute font-['founders'] uppercase translate-x-1/2 top-1/2 -translate-y-1/2 overflow-hidden flex">
                 {"vise".split("").map((item, index) => (
+                  // eslint-disable-next-line react/jsx-key
                   <motion.span
                     initial={{ y: "100%" }}
                     animate={cards[1]}
@@ -106,7 +109,8 @@ function Features() {
               {/* Buttons */}
               <div className="flex w-full items-center gap-4 py-3">
                 {["agency", "company presentation"].map((item, index) => (
-                  <button className="border-[1px] border-zinc-100 px-5 py-1 rounded-full uppercase font-['Neue_Montreal'] text-[13px] hover:bg-white hover:text-[#222] duration-200">
+                  // eslint-disable-next-line react/jsx-key
+                  <button className="border-[1px] border-zinc-100 px-5 py-1 rounded-full uppercase font-['mont'] text-[13px] hover:bg-white hover:text-[#222] duration-200">
                     {item}
                   </button>
                 ))}
@@ -115,7 +119,58 @@ function Features() {
             </motion.article>
             {/* Cards End */}
           </div>
+          
+
         </div>
+        <div className="w-1/2 h-[80vh]">
+            <div className="flex items-center gap-3 pb-6">
+              <div className="w-3 h-3 bg-[#000] rounded-full"></div>
+              <div>
+                <p className="uppercase">vise</p>
+              </div>
+            </div>
+            {/* Cards Container */}
+            <motion.article
+              onHoverStart={() => handelHover(1)}
+              onHoverEnd={() => handelHoverEnd(1)}
+              className="cardContainer relative w-full h-full bg-red-100 rounded-lg cursor-pointer"
+            >
+              <h1 className="text-9xl text-[#CDEA68] right-full z-[9] absolute font-['founders'] uppercase translate-x-1/2 top-1/2 -translate-y-1/2 overflow-hidden flex">
+                {"vise".split("").map((item, index) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <motion.span
+                    initial={{ y: "100%" }}
+                    animate={cards[1]}
+                    transition={{
+                      ease: [0.2, 1, 0.5, 1],
+                      delay: index * 0.02,
+                    }}
+                    className="inline-block"
+                  >
+                    {item}
+                  </motion.span>
+                ))}
+              </h1>
+              <div className="card w-full h-full overflow-hidden rounded-lg">
+                <img
+                  className="w-full h-full"
+                  src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg"
+                  alt=""
+                />
+              </div>
+              {/* Buttons */}
+              <div className="flex w-full items-center gap-4 py-3">
+                {["agency", "company presentation"].map((item, index) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <button className="border-[1px] border-zinc-100 px-5 py-1 rounded-full uppercase font-['mont'] text-[13px] hover:bg-white hover:text-[#222] duration-200">
+                    {item}
+                  </button>
+                ))}
+              </div>
+              {/* Buttons Ends */}
+            </motion.article>
+            {/* Cards End */}
+          </div>
       </div>
     </div>
   );
--- src/components/Ready.jsx
+++ src/components/Ready.jsx
@@ -1,29 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Ready() {
+  const [rotate, setRoatate] = useState(0)
+    
+    useEffect(()=>{
+      window.addEventListener("mousemove", (e)=>{
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+        
+        let deltaX = mouseX - window.innerWidth/2;
+        let deltaY = mouseY - window.innerHeight/2;
+
+        var angle = Math.atan2(deltaY, deltaX) * (180/Math.PI);
+        setRoatate(angle-180);
+        
+      })
+    })
   return (
     <div  data-scroll
     data-scroll-option
-    data-scroll-speed="-.1" className=' w-full py-44 bg-[#CDEA68] flex items-center justify-center relative '>
+    data-scroll-speed="-.1" className=' w-full py-48 bg-[#CDEA68] flex items-center justify-center relative '>
         
-            <h1 className=' font-["founders"] uppercase  text-[17vw]  tracking-tight  mt-16  text-center  leading-[11vw] text-[#212121]'>ready <br /> to start <br /> the projects?</h1>
+            <h1 className=' font-["founders"] uppercase  text-[17vw]  tracking-tight  mt-4  text-center  leading-[11vw] text-[#212121]'>ready <br /> to start <br /> the projects?</h1>
             
              <div className=' absolute  top-[40vw] '>
-               <div className='flex items-center justify-center  mt-40'> 
-                  <button className=' px-4 py-4  ml-14 uppercase bg-zinc-800 text-white rounded-full flex items-center gap-16'>start the projects
+               <div className='flex items-center justify-center  mt-28'> 
+                  <button className=' px-4 py-4  ml-14 uppercase bg-zinc-800 text-white rounded-full flex items-center gap-10 hover:bg-[#000] transition-all ease-in duration-75'>start the projects
                   <div className=' h-2 w-2 rounded-full bg-white'></div>
                  </button>
                </div> 
-               <h1 className=' text-center uppercase font-["mont"]'>or</h1>
-               <div className='flex items-center justify-center  mt-4'> 
-                  <button className=' px-4 py-4  ml-14 uppercase  bg-transparent border border-black text-white rounded-full flex items-center gap-16'>hello2@ochi.design
-                  <div className=' h-2 w-2 rounded-full bg-black'></div>
+               <h1 className=' text-center uppercase font-["mont"] mt-2 ml-10'>or</h1>
+               <div className='flex items-center justify-center  mt-2'> 
+                  <button className=' px-4 py-4  ml-14 uppercase  bg-transparent border border-black  text-black rounded-full flex items-center gap-8 hover:bg-[#000] transition-all ease-[cubic-bezier(0,1,1,0)] duration-100 hover:text-white'>hello2@ochi.design
+                  <div className=' h-2 w-2 rounded-full bg-black hover:bg-white'></div>
                  </button>
                </div> 
 
              </div>
-             <div className=' overlay w-full h-full bg-[#23251900] absolute'>
-                jjdjjdncj
+             <div data-scroll data-scroll-section data-scroll-speed=".7"  className=' overlay w-full h-full bg-[#23251900] absolute'>
+             <div className=' flex items-center  gap-10    absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
+                <div className=' flex items-center justify-center w-[13vw] h-[13vw] bg-slate-100 rounded-full'>
+                    <div className=' w-2/3 h-2/3  relative  bg-[#212121] rounded-full'>
+                        <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className='line  w-full h-7  rotate-3 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
+                        <div className='h-6 w-6 bg-zinc-100 rounded-full'></div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className=' w-[13vw] h-[13vw] flex items-center justify-center bg-slate-100 rounded-full'>
+                    <div className=' w-2/3 h-2/3 relative bg-[#212121] rounded-full'>
+                    <div style={{transform: `translate(-50%,-50%) rotate(${rotate}deg)`}} className='line  w-full h-7 rotate-3 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
+                        <div className='h-6 w-6 bg-zinc-100 rounded-full'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
              </div>
         
     </div>
